701c71cf49b1f3472ca06b31fda6b76e5aea7a85
Surajarya130 <you@example.com>
2021-02-15 04:47:17 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>Zunaso</title>
+</head>
+<body>
+
+<div class="surajContainer">
+    <section class="workOrderManagement">
+        <div class="textBox">
+        <p class="WorkMgmtText">WORK ORDER MANAGEMENT</p>
+        </div>
+        <div class="container">
+            <div class="leftContent">   
+                <h2>Increase Your Equipment Reliability And Reduce Maintenance Costs</h2>
+                <p>“Zunaso’s work order software lets you easily work within your maintenance management system.”</p>
+                <div class="leftListItems">
+                    <div class="leftLi">
+                        <ul>
+                            <li>Automated Rules</li>
+                            <li>Email Notifications</li>
+                            <li>Signature Feature</li>
+                            <li>Attachments and Notes</li>
+                        </ul>
+                    </div>
+                    <div class="rightLi">
+                        <ul>
+                            <li>Checklist</li>
+                            <li>Kits Management</li>
+                            <li>Estimates</li>
+                            <li>Built-In Barcode Scanner</li>
+                        </ul>                
+                    </div>                
+                </div>
+                <p class="learnMore">Learn More &nbsp; <img src="./img/learnArr.png" alt="Learn More" /></p>
+            </div>
+
+            <div class="rightContent">
+                <img src="./img/miniMobile.png" alt="Mini Mobile">
+            </div>
+        </div>
+    </section>
+
+    <section class="useCase">
+        <div class="caseContainer">
+            <div class="bgArea">
+                <img src="./img/leftImg1.jpg" class="bgImg" alt="">
+                <div class="textWrapLeft">
+                    <h1 class="ucHeading">Manufacturing</h1>
+                    <p>Ensure safety, productivity and overall equipment effectiveness within your plant through easy work order management and effective analytics.</p>
+                    <h2 class="learnMore secLearnMore">Learn More  <img src="./img/learnArr.png" alt="Learn More" /></h2>
+                 </div>
+            </div>
+
+            <div class="bgArea">
+            <img src="./img/rightImg1.jpg" class="bgImg" alt="">
+                <div class="textWrapRight">
+                    <h1 class="ucHeading">Education</h1>
+                    <p>Streamline the process of reactive maintenance and avoid manual interventions for all equipment in the School District.</p>
+                    <h2 class="learnMore secLearnMore">Learn More  <img src="./img/learnArr.png" alt="Learn More" /></h2>
+                </div>
+            </div>
+
+            <div class="bgArea">
+            <img src="./img/leftImg2.jpg" class="bgImg" alt="">
+                <div class="textWrapLeft">
+                    <h1 class="ucHeading">Healthcare</h1>
+                    <p>Unify and automate record-keeping and tracking Asset Maintenance, Inventory, and costs with its simplified features.</p>
+                    <h2 class="learnMore secLearnMore">Learn More  <img src="./img/learnArr.png" alt="Learn More" /></h2>
+                </div>
+            </div>
+
+            <div class="bgArea">
+            <img src="./img/rightImg2.jpg" class="bgImg" alt="">
+                <div class="textWrapRight">
+                    <h1 class="ucHeading">Gym & Fitness</h1>
+                    <p>Streamline your work orders for technicians, and view updates by simply scanning your assets using the barcode scanner.</p>
+                    <h2 class="learnMore secLearnMore">Learn More  <img src="./img/learnArr.png" alt="Learn More" /></h2>
+                </div>
+            </div>
+
+            <div class="bgArea">
+            <img src="./img/leftImg3.jpg" class="bgImg" alt="">
+                <div class="textWrapLeft">
+                    <h1 class="ucHeading">Auto Repair</h1>
+                    <p>Enhance tracking, inventory and cost management at the Auto Repair Shop by ensuring resource utilization through barcode scanning, while avoiding manual interventions.</p>
+                    <h2 class="learnMore secLearnMore">Learn More  <img src="./img/learnArr.png" alt="Learn More" /></h2>
+                </div>
+            </div>
+
+            <div class="bgArea">
+            <img src="./img/rightImg3.jpg" class="bgImg" alt="">
+                <div class="textWrapRight">
+                    <h1 class="ucHeading">Cleaning & Housekeeping</h1>
+                    <p>Prevent unnecessary costs and client turnover, by streamlining your cleaning services with efficient planned maintenance tasks.</p>
+                    <h2 class="learnMore secLearnMore">Learn More  <img src="./img/learnArr.png" alt="Learn More" /></h2>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="footerSection">
+        <div class="footer">
+            <div class="footerBlock">
+                <div class="zunasoFooterLogo">
+                    <img class="downLoadIcon" src="img/zunFooterLogo.png" alt="">
+                    <img class="downLoadIcon" src="img/gStore.png" alt="">
+                    <img class="downLoadIcon" src="img/iStore.png" alt="">
+                </div>
+            </div>
+            <div class="footerBlock">
+                <p class="footerTitle">GENERAL</p>
+                <p class="footerLi">About Us</p>
+                <p class="footerLi">Pricing & Packages</p>
+                <p class="footerLi">Blogs</p>
+                <p class="footerLi">FAQ</p>
+                <p class="followUsTitle">FOLLOW US</p>
+                <div class="iconSmallImg">
+                    <img src="img/icons/facebook.png" alt="">
+                    <img src="img/icons/twitter.png" alt="">
+                    <img src="img/icons/linkedIn.png" alt="">
+                    <img src="img/icons/youtube.png" alt="">
+                </div>
+            </div>
+            <div class="footerBlock">
+                <p class="footerTitle">FEATURES</p>
+                <p class="footerLi">Work Order Management</p>
+                <p class="footerLi">Asset Management</p>
+                <p class="footerLi">Inventory Management</p>
+                <p class="footerLi">Maintenance Management</p>
+                <p class="footerLi">Built- In Barcode Scanner</p>
+                <p class="footerLi">Reports</p>
+            </div>
+            <div class="footerBlock">
+                <p class="footerTitle">USE CASES</p>
+                <p class="footerLi">Manufacturing</p>
+                <p class="footerLi">Education</p>
+                <p class="footerLi">Health Care</p>
+                <p class="footerLi">Gym & Fitness</p>
+                <p class="footerLi">Auto Repair</p>
+                <p class="footerLi">Cleaning & Housekeeping</p>
+            </div>
+            <div class="footerBlockForm">
+                <div class="contactCard">
+                    <div class="contactUsForm">
+                        <p class="footerTitle">CONTACT US</p>
+                        <form action="#">
+                            <div class="formControl">
+                                <input type="text" name="name" placeholder="Name" required>
+                            </div>
+                            <div class="formControl">
+                                <input type="email" name="email" placeholder="Email" required>
+                            </div>
+                            <div class="formControl">
+                                <input type="number" name="mobileNumber" placeholder="Phone">
+                            </div>
+                            <div class="formControl">
+                                <input type="text" name="subject" placeholder="Subject">
+                            </div>
+                            <div class="formControl">
+                                <input type="text" name="message" placeholder="Message">
+                            </div>
+                            <div class="formControl">
+                                <p class="requiredText">* All fields are required</p>
+                            </div>
+                            <div class="formControl">
+                                <input class="sendBtn" type="button" value="Send">
+                            </div>                            
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</div>
+</body>
+</html>
